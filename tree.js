fbd4ea54bed5691eafd235f3047fb66d7d473ca2
Sean Gibson <seanthomasgibson@gmail.com>
2021-04-19 22:17:10 -0700

    Implemented 3 functions in tree.js. Implemented sumValues(), countEvens(), & numGreater(lowerBound) in tree.js.

--- tree.js
+++ tree.js
@@ -15,20 +15,66 @@ class Tree {
   /** sumValues(): add up all of the values in the tree. */
 
   sumValues() {
-    
+    // base case for empty tree
+    if (!this.root) {
+      return 0;
+    }
+
+    const nodeStack = [this.root];
+    let sum = 0;
+    while (nodeStack.length) {
+      const currNode = nodeStack.pop();
+      sum += currNode.val;
+      for (let child of currNode.children) {
+        nodeStack.push(child);
+      }
+    }
+    return sum;
   }
 
   /** countEvens(): count all of the nodes in the tree with even values. */
 
   countEvens() {
+    // base case for empty tree
+    if (!this.root) {
+      return 0;
+    }
 
+    const nodeStack = [this.root];
+    let count = 0;
+    while (nodeStack.length) {
+      const currNode = nodeStack.pop();
+      if (currNode.val % 2 === 0) {
+        count++;
+      }
+      for (let child of currNode.children) {
+        nodeStack.push(child);
+      }
+    }
+    return count;
   }
 
   /** numGreater(lowerBound): return a count of the number of nodes
    * whose value is greater than lowerBound. */
 
   numGreater(lowerBound) {
+    // base case for empty tree
+    if (!this.root) {
+      return 0;
+    }
 
+    const nodeStack = [this.root];
+    let count = 0;
+    while (nodeStack.length) {
+      const currNode = nodeStack.pop();
+      if (currNode.val > lowerBound) {
+        count++;
+      }
+      for (let child of currNode.children) {
+        nodeStack.push(child);
+      }
+    }
+    return count;
   }
 }
 
